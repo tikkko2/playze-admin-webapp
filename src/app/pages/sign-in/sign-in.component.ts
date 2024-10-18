ccb34bcfb780e30f1140d1296de56345794e2b6b
Tiko <tikoafri2002@gmail.com>
2024-10-18 19:56:23 +0400

    added login

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { AuthModel } from '../../core/models/auth.model';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
+  private _authService = inject(AuthService);
+  loginForm!: FormGroup;
 
+  constructor(
+    private _builder: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.loginForm = this._builder.group({
+      email: this._builder.control(``, Validators.required),
+      password: this._builder.control(``, Validators.required),
+    });
+  }
+
+  proceedLogin() {
+    const model: AuthModel = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    }
+    this._authService.authorize(model).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    )
+  }
 }
