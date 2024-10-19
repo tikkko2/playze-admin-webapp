7d5a55eae3a6b7386e332a6ac445937e07812d4f
Lasha Shavgulidze <shavg@LASHA>
2024-10-19 13:04:23 +0400

    Feature/Header-Improvments

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NavigationService } from '../../core/services/navigation.service';
 
 @Component({
   selector: 'app-news',
   standalone: true,
   imports: [],
   templateUrl: './news.component.html',
-  styleUrl: './news.component.scss'
+  styleUrl: './news.component.scss',
 })
 export class NewsComponent {
-
+  public _navigationService = inject(NavigationService);
 }
