97d1642aeff6324407947901e093d423af11131d
Datodarchiashvili123 <datodarchiashvili@gmail.com>
2024-11-17 18:59:32 +0400

    change envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7261/api',
+  apiUrl: 'https://playze-admin.betterdevjobs.com/api',
   bucket: 'https://playze-uploads.s3.us-east-1.amazonaws.com/',
 };
+
